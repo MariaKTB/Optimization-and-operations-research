9c531bda06a1b917189a9076cb26af54adb89cde
MariaKTB <98776869+MariaKTB@users.noreply.github.com>
2024-02-19 22:21:14 +0100

    Update subset_sum.py

--- subset_sum.py
+++ subset_sum.py
@@ -9,7 +9,6 @@ def print_subset_sum(i, n, _set, target_sum, subset):
     global flag
     # If targetSum is zero, then there exists a subset
     if target_sum == 0:
-        # Prints valid subset
         flag = True
         print("[", end=" ")
         for element in subset:
@@ -18,21 +17,15 @@ def print_subset_sum(i, n, _set, target_sum, subset):
         return
 
     if i == n:
-        # Return if we have reached the end of the array
         return
 
-    # Not considering the current element
     print_subset_sum(i + 1, n, _set, target_sum, subset)
 
-    # Consider the current element if it is less than or equal to targetSum
     if _set[i] <= target_sum:
-        # Push the current element into the subset
         subset.append(_set[i])
 
-        # Recursive call for considering the current element
         print_subset_sum(i + 1, n, _set, target_sum - _set[i], subset)
 
-        # Remove the last element after recursive call to restore subset's original configuration
         subset.pop()
 
 def subset_sum_time(set_):
@@ -56,14 +49,13 @@ def subset_sum_time(set_):
             subset.pop()
 
     subset = []
-    target_sum = sum(set_) // 2  # We're looking for subsets with a sum equal to half of the total sum
+    target_sum = sum(set_) // 2  
     start_time = time.time()
     print_subset_sum(0, len(set_), set_, target_sum, subset)
     end_time = time.time()
 
     return end_time - start_time
 
-# Driver code
 if __name__ == "__main__":
     # Test case 1
     set_1 = [1, 2, 1]
@@ -75,7 +67,6 @@ if __name__ == "__main__":
     print()
     flag = False
 
-    # Test case 2
     set_2 = [3, 34, 4, 12, 5, 2]
     sum_2 = 30
     n_2 = len(set_2)
@@ -85,7 +76,7 @@ if __name__ == "__main__":
     if not flag:
         print("There is no such subset")
     
-    num_cities_list = [4, 5, 6, 7, 8]  # Example: Test cities from 4 to 8
+    num_cities_list = [4, 5, 6, 7, 8]  
     time_spent_list = []
 
     for num_cities in num_cities_list:
@@ -93,7 +84,6 @@ if __name__ == "__main__":
         time_spent = subset_sum_time(set_)
         time_spent_list.append(time_spent)
 
-    # Plotting
     plt.plot(num_cities_list, time_spent_list, marker='o', color='b')
     plt.title('Time Spent by Algorithm vs. Number of Cities')
     plt.xlabel('Number of Cities')
